698aa22cc5b252de971ea196f795359e391da9ea
xbrems <xbrems@STRESSIOPC10.office.frm2>
2019-05-09 13:09:54 +0200

    modified grasp_startup_bayes so one has to manually start grasp

--- New_Grasp_v8.07_code/grasp_startup_bayes_new.m
+++ New_Grasp_v8.07_code/grasp_startup_bayes_new.m
@@ -9,6 +9,6 @@ disp('Adding override paths for Bayesian stuff');
 %
 
 addpath(fullfile(bayes_root,'GRASP_overrides_v8.07')); 
-addpath(genpath(fullfile(bayes_root,'minFunc_2012'))); 
-
-grasp
+addpath(genpath(fullfile(bayes_root,'minFunc_2012')));
+addpath(bayes_root);
+disp('Run grasp.m as usual now');
